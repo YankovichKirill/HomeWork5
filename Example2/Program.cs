653cf2aec67b7ccf714d663dadbc4fefcceeea3e
YankovichKirill <kirill.best97@gmail.com>
2022-09-07 21:17:29 +0300

    Save2

---
+++ Example2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write($"Enter the number of array elements: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("The resulting array: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+Console.WriteLine($"\nSum of elements in odd positions: {randomNumbers}");
