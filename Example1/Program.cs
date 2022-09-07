259352877b24435ac89198168f218ced46da6e9d
YankovichKirill <kirill.best97@gmail.com>
2022-09-07 20:28:30 +0300

    Save1

---
+++ Example1/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] numbers = new int[10];
+
+void FillArray(int[] array, int min, int max){
+    for (int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void WriteArray(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write(array[i] + " ");
+    }
+}
+
+int QuantityPositive(int[] array){
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++ ){
+        if (array[i] % 2 == 1)
+        {
+            quantity++;
+        }
+    }
+    return quantity;
+}
+
+FillArray(numbers, 100, 1000);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"The number of even numbers in the array: {quantity}");
